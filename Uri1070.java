c9a0eabcc6627cee23ab287224a62b9f5f110d98
wilsonviana3274 <wrsviana@gmail.com>
2020-05-28 17:22:52 -0300

    primeira versao

---
+++ Uri1070.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Uri1142{
+    public static void main(String args[]{
+        Scanner teclado = new Scanner(System.in);
+        
+        int inicio;
+        inicio = teclado.nextInt();
---
+++ Uri1142.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+public class Uri1142{
+    public static void main(String args[]{
+        Scanner teclado = new Scanner(System.in);
+        
